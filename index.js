8fbb97cf9a2537262dce05c573e0cd2ac64c236f
Jan Kuehle <jkuehle90@gmail.com>
2018-11-22 23:30:07 +0000

    Add useState

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+let currentStateStore = null;
+
+function useState(initialValue) {
+    if (currentStateStore === null) {
+        throw new Error("Cannot call useState outside of render.");
+    }
+
+    currentStateStore.counter++;
+
+    if (currentStateStore.firstRender) {
+        currentStateStore.state.push([
+            initialValue,
+            createStateSetter(
+                currentStateStore.state,
+                currentStateStore.counter,
+                currentStateStore.onStateChange
+            )
+        ]);
+    }
+
+    return currentStateStore.state[currentStateStore.counter];
+}
+
+function createStateSetter(state, counter, onStateChange) {
+    return function(value) {
+        if (currentStateStore !== null) {
+            throw new Error("Cannot set state during render.");
+        }
+
+        state[counter][0] = value;
+        onStateChange();
+    };
+}
+
 function makeWebComponent(functionalComponent) {
     const webComponent = class extends HTMLElement {
         constructor() {
             super();
+            this._stateStore = {
+                firstRender: true,
+                counter: -1,
+                state: [],
+                onStateChange: this._render.bind(this)
+            };
             this._props = {};
             this.attachShadow({ mode: "open" });
         }
@@ -11,7 +51,14 @@ function makeWebComponent(functionalComponent) {
         }
 
         _render() {
-            this.shadowRoot.innerHTML = functionalComponent(this._props);
+            try {
+                currentStateStore = this._stateStore;
+                this.shadowRoot.innerHTML = functionalComponent(this._props);
+            } finally {
+                currentStateStore = null;
+                this._stateStore.firstRender = false;
+                this._stateStore.counter = -1;
+            }
         }
     };
 
@@ -46,4 +93,4 @@ function makeWebComponent(functionalComponent) {
     return webComponent;
 }
 
-export { makeWebComponent };
+export { makeWebComponent, useState };
